beac94c892c0844d8a5cde8d05d4c644c787d72e
12ZainTahir <zaintahir0012@gmail.com>
2022-07-10 17:13:04 +0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -33,7 +33,7 @@ router.get('/login', function(req, res, next) {
   res.render('login');
 });
 router.post('/login', async function(req, res, next) {
-  let user = await User.findOne({np
+  let user = await User.findOne({
     email:req.body.email,
     password:req.body.password
   });
